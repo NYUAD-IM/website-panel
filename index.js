2228786342d0746b22b83cfe8627ed016d6b75cb
Kotavy <tavius.koktavy@gmail.com>
2018-02-17 22:44:59 +0400

    View JSON via Heroku

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require('express')
+const fs = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var file = ''
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
+  .get('/', (req, res) => res.send('Welcome the NYUAD.IM Heroku home. Visit /api/[people, workshops, academics] to view the JSON'))
+  .get('/api/*', jsonLoad)
   // Third param as error handling
-  .get('/', (req, res) => res.send('Hello'))
+  // .get('/edit/*', jsonEdit, errorFunc)
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// fs.readfile()
-// json.parse
-//
+// Pull JSON data based on url request, serve to client:
+function jsonLoad(req, res) {
+    cutPath(req.url);
+        fs.readFile("docs/data/" + file + ".json", function(err, inData) {
+            if(err) {
+                res.send("400, Bad Request");
+            }
+            var outData = JSON.parse(inData);
+            res.send(outData);
+        });
+}
 
-// /api/json to show data
-// /edit to allow editing
-// / for home
+// TODO:
+// function jsonEdit() {
+//
+// }
+
+function cutPath(url) {
+    var urlBits = url.split('/');
+    var wantedBit = urlBits[urlBits.length - 1];
+    file = wantedBit;
+}
