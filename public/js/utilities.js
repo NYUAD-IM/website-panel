797ec84bed71937b01939b074c437442b3da93a1
Kotavy <tavius.koktavy@gmail.com>
2018-03-22 17:36:18 +0400

    Fix https

--- public/js/utilities.js
+++ public/js/utilities.js
@@ -41,7 +41,7 @@ let listParse = (obj, populate, list) => {
 let send = (_data) => {
   $.ajax({
     // 'http://localhost:5000/data/' +
-    url: 'http://imheroku.herokuapp.com/save/' + document.getElementById('category').innerText,
+    url: 'https://imheroku.herokuapp.com/save/' + document.getElementById('category').innerText,
     method: 'POST',
     data: _data,
     dataType: 'json'
